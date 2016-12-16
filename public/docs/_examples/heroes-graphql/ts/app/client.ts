74a26ff2ea088d4fd4a6c61b282db08795330ee0
urigo <uri.goldshtein@gmail.com>
2016-12-16 17:04:24 +0200

    docs(cookbook/graphql): Add store id resolution

--- public/docs/_examples/heroes-graphql/ts/app/client.ts
+++ public/docs/_examples/heroes-graphql/ts/app/client.ts
@@ -3,6 +3,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { networkInterface } from './in-memory-graphql';
 
 export const client = new ApolloClient({
-  networkInterface
+  networkInterface,
+  dataIdFromObject: (object:any) => object.id,
 });
 // #enddocregion
